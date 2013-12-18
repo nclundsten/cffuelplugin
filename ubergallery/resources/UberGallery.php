f9c8cebfd2c25d961e870b1f0fe821b7d5990dab
Blaž Hrastnik <speed.the.bboy@gmail.com>
2013-12-18 17:36:36 +0100

    remove unused options

--- ubergallery/resources/UberGallery.php
+++ ubergallery/resources/UberGallery.php
@@ -245,42 +245,6 @@ class UberGallery {
 
     }
 
-
-    /**
-     * Returns the theme name
-     *
-     * @return string Theme name as set in user config
-     * @access public
-     */
-    public function getThemeName() {
-        // Return the theme name
-        return $this->_config['theme_name'];
-    }
-
-
-    /**
-     * Returns the path to the chosen theme directory
-     *
-     * @param bool $absolute true = return absolute path / false = return relative path (default)
-     * @return string Path to theme
-     * @access public
-     */
-    public function getThemePath($absolute = false) {
-        if ($absolute) {
-            // Set the theme path
-            $themePath = $this->_appDir . '/themes/' . $this->_config['theme_name'];
-        } else {
-            // Get relative path to application dir
-            $realtivePath = $this->_getRelativePath(getcwd(), $this->_appDir);
-
-            // Set the theme path
-            $themePath = $realtivePath . '/themes/' . $this->_config['theme_name'];
-        }
-
-        return $themePath;
-    }
-
-
     /**
      * Get an array of error messages or false when empty
      *
